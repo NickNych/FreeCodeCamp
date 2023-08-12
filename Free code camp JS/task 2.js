fbcf10891f2de06f1778a8ac0039d0e1d4efea85
Nick <nikolpaladiy@gmail.com>
2023-08-11 23:03:54 -0400

    refactor: add couple comments in task 2

--- Free code camp JS/task 2.js
+++ Free code camp JS/task 2.js
@@ -12,7 +12,7 @@ for (var i = 0; i < 3; i++) {
 }
 console.log(numArray);
 console.log(i);
-Here the console will display the values [0, 1, 2] and 3.
+// Here the console will display the values [0, 1, 2] and 3.
 
 // With the var keyword, i is declared globally. So when i++ is executed, it updates the global variable. This code is similar to the following:
 var numArray = [];
@@ -22,7 +22,8 @@ for (i = 0; i < 3; i++) {
 }
 console.log(numArray);
 console.log(i);
-Here the console will display the values [0, 1, 2] and 3.
+
+// Here the console will display the values [0, 1, 2] and 3.
 
 // This behavior will cause problems if you were to create a function and store it for later use inside a for loop that uses the i variable. This is because the stored function will always refer to the value of the updated global i variable.
 var printNumTwo;
@@ -34,7 +35,7 @@ for (var i = 0; i < 3; i++) {
   }
 }
 console.log(printNumTwo());
-Here the console will display the value 3.
+// Here the console will display the value 3.
 
 // As you can see, printNumTwo() prints 3 and not 2. This is because the value assigned to i was updated and the printNumTwo() returns the global i and not the value i had when the function was created in the for loop. The let keyword does not follow this behavior:
 let printNumTwo;
