5213293848447990f7be05369d8c0d5321941c2b
Nick <nikolpaladiy@gmail.com>
2023-08-11 23:19:04 -0400

    refactor: change names js tasks

--- Free code camp JS/task 1.js
+++ Free code camp JS/task 1.js
@@ -1,22 +1,68 @@
-//  Use Destructuring Assignment to Assign Variables from Arrays
-// ES6 makes destructuring arrays as easy as destructuring objects.
+// Compare Scopes of the var and let Keywords
+// If you are unfamiliar with let, check out this challenge about the difference between let and var.
 
-// One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list. Consequently, you cannot pick or choose which elements you want to assign to variables.
+// When you declare a variable with the var keyword, it is declared globally, or locally if declared inside a function.
 
-// Destructuring an array lets us do exactly that:
+// The let keyword behaves similarly, but with some extra features. When you declare a variable with the let keyword inside a block, statement, or expression, its scope is limited to that block, statement, or expression.
 
-// const [a, b] = [1, 2, 3, 4, 5, 6];
-// console.log(a, b);
-// The console will display the values of a and b as 1, 2.
+// For example:
+var numArray = [];
+for (var i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray);
+console.log(i);
+// Here the console will display the values [0, 1, 2] and 3.
 
-// The variable a is assigned the first value of the array, and b is assigned the second value of the array. We can also access the value at any index in an array with destructuring by using commas to reach the desired index:
+// With the var keyword, i is declared globally. So when i++ is executed, it updates the global variable. This code is similar to the following:
+var numArray = [];
+var i;
+for (i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray);
+console.log(i);
 
-// const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
-// console.log(a, b, c);
-// The console will display the values of a, b, and c as 1, 2, 5.
+// Here the console will display the values [0, 1, 2] and 3.
 
-// Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
-let a = 8, b = 6;
-// Змініть код лише під цим рядком
-[a,b] = [b,a];
-console.log(a, b);
+// This behavior will cause problems if you were to create a function and store it for later use inside a for loop that uses the i variable. This is because the stored function will always refer to the value of the updated global i variable.
+var printNumTwo;
+for (var i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+// Here the console will display the value 3.
+
+// As you can see, printNumTwo() prints 3 and not 2. This is because the value assigned to i was updated and the printNumTwo() returns the global i and not the value i had when the function was created in the for loop. The let keyword does not follow this behavior:
+let printNumTwo;
+for (let i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+console.log(i);
+
+// Here the console will display the value 2, and an error that i is not defined.
+
+// i is not defined because it was not declared in the global scope. It is only declared within the for loop statement. printNumTwo() returned the correct value because three different i variables with unique values (0, 1, and 2) were created by the let keyword within the loop statement.
+
+// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+
+// This exercise is designed to illustrate the difference between how var and let keywords assign scope to the declared variable. When programming a function similar to the one used in this exercise, it is often better to use different variable names to avoid confusion.
+
+function checkScope() {
+	let i = 'function scope';
+	if (true) {
+	  let i = 'block scope';
+	  console.log('Block scope i is: ', i);
+	}
+	console.log('Function scope i is: ', i);
+	return i;
+ }
